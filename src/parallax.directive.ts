9e527a21bb796ca70e5677be9e1e0dc14f2371a3
Ibby Hadeed <ibby93@gmail.com>
2017-05-04 08:47:19 -0400

    different approach

--- src/parallax.directive.ts
+++ src/parallax.directive.ts
@@ -1,62 +1,36 @@
-import { Directive, Input, OnInit, OnDestroy, ElementRef, Renderer2 } from '@angular/core';
-import { Content, DomController } from 'ionic-angular';
+import { Directive, Input, OnInit, AfterViewInit, OnDestroy, ElementRef, Renderer2 } from '@angular/core';
+import { Content, DomController, Platform } from 'ionic-angular';
 import { Subscription } from 'rxjs/Subscription';
 
 @Directive({
-    selector: '[parallax]',
+    selector: '[parallax]'
 })
-export class ParallaxDirective implements OnInit, OnDestroy {
+export class ParallaxDirective implements OnInit, AfterViewInit, OnDestroy {
 
     @Input()
-    ratio: number = 80;
-
-    @Input()
-    rootElement: HTMLElement;
-
-    @Input()
-    nodes: number;
+    ratio: number = 40;
 
     private _watch: Subscription;
+
+    private _originalHeight: number;
 
     constructor(
         private _content: Content,
         private _el: ElementRef,
         private _rnd: Renderer2,
-        private _domCtrl: DomController
-    ) {}
+        private _domCtrl: DomController,
+        private _plt: Platform
+    ) { }
 
     ngOnInit() {
-        let el: HTMLElement = this.getNativeElement();
-
-        // We figure out everything by measuring the distance between
-        // <ion-content> and the element this directive is attached to.
-
-        // In case this element is not a direct child of <ion-content>,
-        // we need to specify the number of parent nodes separating this
-        // element from <ion-content>.
-
-        // For example:
-        // <ion-content>
-        //   <ion-card>
-        //      <div parallax [nodes]="1" style="background-url: url(...)"></div>
-        //   </ion-card>
-        // </ion-content>
-
-        if (!!this.nodes && !isNaN(this.nodes) && this.nodes > 0) {
-            for (let i = 0; i < this.nodes; i++) {
-                try {
-                    el = el.parentElement;
-                } catch (e) {
-                    // this shouldn't happen unless if the developer misconfigured this element
-                    // use any element as a fallback... it will not really work but at least it won't throw errors.
-                    el = this.getNativeElement();
-                }
-            }
-        }
-
-        this.rootElement = el;
         this.listen();
-        this._onScroll(null, true);
+    }
+
+    ngAfterViewInit() {
+        this._plt.timeout(() => {
+            // TODO figure out why the height is not correct unless if we wait a little bit ...
+            this._onScroll(null, true);
+        }, 500);
     }
 
     ngOnDestroy() {
@@ -75,19 +49,16 @@ export class ParallaxDirective implements OnInit, OnDestroy {
         return this._el.nativeElement;
     }
 
-    getRootElement(): HTMLElement {
-        return this.rootElement;
-    }
-
     private _onScroll(ev: any, force: boolean = false) {
 
         this._domCtrl.read(() => {
-
             const
-                viewHeight: number = this._content.scrollHeight,
-                distanceYtoBottom: number = this.getRootElement().offsetHeight + this.getRootElement().offsetTop - this._content.scrollTop,
-                distanceYtoTop: number = this.getRootElement().offsetTop - this._content.scrollTop,
-                distanceYtoCenter: number = distanceYtoTop + this.getRootElement().offsetHeight / 2
+                el: HTMLElement = this.getNativeElement(),
+                clientRect: ClientRect = el.getBoundingClientRect(),
+                contentTop: number = this._content.contentTop,
+                viewHeight: number = this._plt.height() - this._content.contentBottom - this._content.contentTop,
+                distanceYtoBottom: number = clientRect.bottom - contentTop,
+                distanceYtoTop: number = clientRect.top - contentTop
             ;
 
             if (!force && (distanceYtoBottom < 0 || distanceYtoTop > viewHeight)) {
@@ -95,19 +66,27 @@ export class ParallaxDirective implements OnInit, OnDestroy {
                 return;
             }
 
-            // magic formula
-            // figure out how much should we move the image from 50%
-            let y = (viewHeight - distanceYtoCenter) / viewHeight * this.ratio / 2 + 50;
-
-            // make sure it's > 0 && < 100
-            y = Math.min(100, Math.max(y, 0));
-
-            // update the DOM!
             this._domCtrl.write(() => {
-                this._rnd.setStyle(this.getNativeElement(), 'background-position', `center ${y}%`);
+
+                if (!this._originalHeight) {
+                    const maxY = (viewHeight + (this._originalHeight = el.offsetHeight)) / viewHeight * this.ratio / 100;
+                    this._rnd.setStyle(this.getNativeElement(), 'height', this._originalHeight * (maxY + 1) + 'px');
+                    this._rnd.setStyle(this.getNativeElement(), 'background-position-x', '50%');
+                }
+
+                // magic formula
+                // figure out how much should we move the image from 50%
+                let y = (viewHeight - distanceYtoTop) / viewHeight * this.ratio / 100;
+
+                // make sure it's > 0 && < 100
+                y = Math.min(1, Math.max(y, 0));
+
+                // update the DOM!
+                this._rnd.setStyle(el, this._plt.Css.transform, `translate3d(0, -${ y * this._originalHeight }px, 0)`)
+
             });
 
-        });
+        })
 
     }
 
